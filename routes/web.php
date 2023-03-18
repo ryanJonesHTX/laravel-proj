9d8449c7ecf9bc15f35afc064ad447f4db1304a2
Ryan Jones <RyanJonesHTX@pm.me>
2023-03-18 01:02:58 -0500

    added form and validation, updated routing, controller, and model

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,17 +16,25 @@ use App\Models\Listing;
 |
 */
 
+// Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing  
+
 //All listings
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
+
+
+//Show Create Form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+//Store Listing Controller
+Route::post('/listings', [ListingController::class, 'store']);
+
 
 //Single listing
-Route::get('/listings/{listing}', function (Listing $listing) {
-    return view('listing', [
-        'listing' => $listing
-    ]);
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
